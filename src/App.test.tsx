a5857f4130505f17f85fdec27c1406c4ac11fa84
Joao Victor Dias <victorcostadev@gmail.com.br>
2024-07-13 12:44:02 -0300

    improve App.tsx test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders app component', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(screen.getByText(/todo list/i)).toBeInTheDocument();
+  expect(screen.getByText(/login/i)).toBeInTheDocument();
 });
